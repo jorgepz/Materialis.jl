81fd8169ea92106cc5a6f856f0c27bfdc000dfdb
jorgepz <jorgepz@fing.edu.uy>
2022-06-01 16:21:25 -0300

    agrego ejemplo grid

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using Materialis, WriteVTK
+using Materialis, WriteVTK, SparseArrays
 using Test
 
 @testset "Test: FEM2Grid interpolation matrix" begin
@@ -11,13 +11,21 @@ using Test
 
     testGrid = intGrid( [3,3,3], [0., 0., 0.], 0.3*ones(3) )
 
-    computeFEM2GridInterpMatrix( testNodes, testConec, testGrid, 3, 0 )
+    interpMatrix = computeFEM2GridInterpMatrix( testNodes, testConec, testGrid, 3, 0 )
 
+    print("AHHHHH:", Matrix(interpMatrix) )
     # paraview output
     cells = [ MeshCell( VTKCellTypes.VTK_TETRA, testConec[1] ) ]
 
     vtk_grid("interpolationExample", Array(testNodes') , cells) do vtk
         vtk["displacements", VTKPointData()] = rand(4,3)
+    end
+
+    x, y, z = 0:10, 1:6, 2:0.1:3
+    Nx, Ny, Nz = length(x), length(y), length(z)
+
+    vtk_grid("fields", x, y, z) do vtk
+      vtk["Pressure"] = rand(Nx - 1, Ny - 1, Nz - 1)   # metadata ("field data" in VTK)
     end
 
     @test 0 < 1
